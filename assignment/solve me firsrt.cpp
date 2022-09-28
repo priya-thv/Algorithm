13aa38d64ffffb335d364b8e5f0d604c4cce68e4
priya <107272239+priya-thv@users.noreply.github.com>
2022-09-28 11:33:48 +0530

    Create solve me firsrt.cpp

---
+++ assignment/solve me firsrt.cpp
@@ -0,0 +1,25 @@
+https://www.hackerrank.com/challenges/solve-me-first/problem?isFullScreen=true
+
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int solveMeFirst(int a, int b) {
+ // Hint: Type return a+b; below:
+  int sum2=0;
+  sum2=a+b;
+  return sum2;
+}
+
+int main() {
+  int num1, num2;
+  int sum;
+  cin>>num1>>num2;
+  sum = solveMeFirst(num1,num2);
+  cout<<sum;
+  return 0;
+}
