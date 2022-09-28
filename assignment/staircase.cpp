e8e4e6b8312db495317f2e0fb0d57cb19856c3b5
priya <107272239+priya-thv@users.noreply.github.com>
2022-09-28 21:49:06 +0530

    Create staircase.cpp

---
+++ assignment/staircase.cpp
@@ -0,0 +1,62 @@
+https://www.hackerrank.com/challenges/staircase/problem?isFullScreen=true
+
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'staircase' function below.
+ *
+ * The function accepts INTEGER n as parameter.
+ */
+
+void staircase(int n) {
+int s=n-1;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(s<=j) cout<<"#";
+            else cout<<" ";
+        }
+        s--;
+        cout<<"\n";
+    }
+
+}
+
+int main()
+{
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    staircase(n);
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
