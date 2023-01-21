026ccd928230bbc97011131de70a93b7feb27f22
priya <107272239+priya-thv@users.noreply.github.com>
2023-01-21 18:14:14 +0530

    Create BSTpostorder.cpp

---
+++ Assignment10/BSTpostorder.cpp
@@ -0,0 +1,31 @@
+https://leetcode.com/problems/binary-tree-postorder-traversal/submissions/881052266/
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+     void postorder(TreeNode*root,vector<int>&ans){
+        if(root==NULL)
+        return ;
+        postorder(root->left,ans);
+        postorder(root->right,ans);
+        ans.push_back(root->val);
+        
+     }
+
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int>ans;
+        postorder(root,ans);
+        return ans;
+    }
+};
