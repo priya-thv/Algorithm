110b7149e5f86ec60a072a7b0e0a4020b741140c
priya <107272239+priya-thv@users.noreply.github.com>
2023-01-21 18:10:31 +0530

    Create insertionleetcode.cpp

---
+++ Assignment10/insertionleetcode.cpp
@@ -0,0 +1,28 @@
+https://leetcode.com/problems/insert-into-a-binary-search-tree/submissions/879814333/
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* insertIntoBST(TreeNode* root, int val) {
+        if(root==NULL){
+           root=new TreeNode(val);
+           return root;
+        }
+        if(root->val>val){
+            root->left= insertIntoBST(root->left,val);
+        }else if(root->val<val){root->right=insertIntoBST(root->right,val);
+        } 
+        return root;
+    }
+};
