7b45b888f1eeb48fe6f14c177c3df835f46745c3
priya <107272239+priya-thv@users.noreply.github.com>
2023-01-26 12:24:19 +0530

    Create assignment 10 question.cpp

---
+++ README.md/assignment 10 question.cpp
@@ -0,0 +1,17 @@
+# Algorithm
+IMPLEMENTATION USING RECURSION:
+1.https://leetcode.com/problems/binary-tree-inorder-traversal/
+2.https://www.hackerrank.com/challenges/tree-inorder-traversal/problem
+3.https://leetcode.com/problems/binary-tree-postorder-traversal/
+4.https://www.hackerrank.com/challenges/tree-postorder-traversal/problem
+5.https://leetcode.com/problems/binary-tree-preorder-traversal/
+6.https://www.hackerrank.com/challenges/tree-preorder-traversal/problem
+IMPLEMENTATION USING STACK:
+7.https://leetcode.com/problems/binary-tree-inorder-traversal/
+8.https://leetcode.com/problems/binary-tree-postorder-traversal/
+9.https://leetcode.com/problems/binary-tree-preorder-traversal/
+MORE QUESTIONS:
+10.https://www.hackerrank.com/challenges/tree-height-of-a-binary-tree/problem?isFullScreen=true
+11.https://leetcode.com/problems/search-in-a-binary-search-tree/
+12.https://www.hackerrank.com/challenges/tree-level-order-traversal/problem?isFullScreen=true
+13.https://leetcode.com/problems/binary-tree-level-order-traversal/
